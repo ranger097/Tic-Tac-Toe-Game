fd20af09ebb7d61dac05dfb56e540b2520dabb4c
ranger <mrgentry13@gmail.com>
2025-07-29 21:31:20 -0400

    removed game props and added data attributes to html to better access data

--- src/Table.js
+++ src/Table.js
@@ -7,19 +7,19 @@ const Tabel = ({game}) => {
         <table>
             <caption><h1>Tic - Tac - Toe</h1></caption>
             <tr>
-                <td><p className="table-content">{game}</p></td>
-                <td><p className="table-content">{game}</p></td>
-                <td><p className="table-content">{game}</p></td>
+                <td data-cell="0" ><p className="table-content"></p></td>
+                <td data-cell="1" ><p className="table-content"></p></td>
+                <td data-cell="2" ><p className="table-content"></p></td>
             </tr>
              <tr>
-                <td><p className="table-content">{game}</p></td>
-                <td><p className="table-content">{game}</p></td>
-                <td><p className="table-content">{game}</p></td>
+                <td data-cell="3" ><p className="table-content"></p></td>
+                <td data-cell="4" ><p className="table-content"></p></td>
+                <td data-cell="5" ><p className="table-content"></p></td>
             </tr>
              <tr>
-                <td><p className="table-content">{game}</p></td>
-                <td><p className="table-content">{game}</p></td>
-                <td><p className="table-content">{game}</p></td>
+                <td data-cell="6" ><p className="table-content"></p></td>
+                <td data-cell="7" ><p className="table-content"></p></td>
+                <td data-cell="8" ><p className="table-content"></p></td>
             </tr>
         </table>
     );
